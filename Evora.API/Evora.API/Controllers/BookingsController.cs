5b0f3d7be477324a1c930501b9b2fbb7b97ba83d
shwetan029 <shwetan275@gmail.com>
2025-10-31 08:36:55 +0530

    testing endpoint added

--- Evora.API/Evora.API/Controllers/BookingsController.cs
+++ Evora.API/Evora.API/Controllers/BookingsController.cs
@@ -1,6 +1,22 @@
-﻿namespace Evora.API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Evora.API.Controllers
 {
-    public class BookingsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookingsController : ControllerBase
     {
+        [HttpGet("get-all-bookings")]
+        public IActionResult GetBookings()
+        {
+            var bookings = new List<object>
+            {
+                new { Id = 1, CustomerName = "John Doe", Date = "2025-11-01", Amount = 2500 },
+                new { Id = 2, CustomerName = "Jane Smith", Date = "2025-11-05", Amount = 1800 },
+                new { Id = 3, CustomerName = "Michael Lee", Date = "2025-11-10", Amount = 3200 }
+            };
+
+            return Ok(bookings);
+        }
     }
 }
